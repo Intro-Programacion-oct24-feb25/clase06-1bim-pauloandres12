205485b9517a5704e7c4abc4618804ceffeb5b24
Paulo Andres <pbustmante@utpl.edu.ec>
2024-11-14 11:39:17 -0500

    Actualizacion 6

--- ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo031.java
+++ ejemplos-switch/EjemplosSwitch/src/paquete01/Ejemplo031.java
@@ -24,10 +24,18 @@ public class Ejemplo031 {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
         
-        System.out.println("Ingrese el nombre de una ciudad del Ecuador");
+        System.out.println("Ingrese su nombre");
         String nombre = entrada.nextLine();
-        // nombre = "atacames"
+        System.out.println("Ingrese su apellido");
+        String apellido = entrada.nextLine();
+        System.out.println("Ingrese su edad");
+        int edad = entrada.nextInt();
+        System.out.println("Ingrese su Universidad");
+        entrada.nextLine();
+        String universidad = entrada.nextLine();
+                
         char valor = nombre.charAt(0); // obtener el primer caracter de una
+        nombre = nombre.toLowerCase();
         // ANA DIAZ con edad 19, es estudiante de UTPL
 
         // luis vera con edad 19, es estudiante de UNL
@@ -38,13 +46,15 @@ public class Ejemplo031 {
             case 'i':
             case 'o':
             case 'u':
-                System.out.printf("Nombre con inicial %s de %s\n", 
-                        valor, nombre.toLowerCase());
+                System.out.printf("%s %s con edad %d, es estudiante de %s\n", 
+                       nombre.toUpperCase(), apellido.toUpperCase(), edad, 
+                       universidad.toUpperCase());
                 break;
             
             default:
-                System.out.println("opción incorrecta; ninguna de las "
-                        + "anteriores");
+                System.out.printf("%s %s con edad %d, es estudiante de %s\n", 
+                       nombre.toLowerCase(), apellido.toLowerCase(), edad, 
+                       universidad.toUpperCase());
                 break;
                 
         }
